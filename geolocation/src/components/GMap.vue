bfe6cff9d831692494a1a3ca9a5ffa72d153627f
Arghun-dev <arghun.developer@gmail.com>
2020-03-19 19:46:31 +0330

    Created GMap component

---
+++ geolocation/src/components/GMap.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="GMap">
+        <h1>GMap</h1>
+    </div>  
+</template>
+
+<script>
+export default {
+    name: 'GMap',
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
